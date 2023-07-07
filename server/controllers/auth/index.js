40fa40217a0b3ae4f6daf845796c71aa77582e0d
Septian Maulana <septianm028@gmail.com>
2023-07-07 21:14:26 +0700

    build: create Users table; feat: create register user endpoint

--- server/controllers/auth/index.js
+++ server/controllers/auth/index.js
@@ -1,7 +1,23 @@
+const { User } = require("../../models");
+
 class Auth {
   static async register(req, res, next) {
     try {
-      console.log(req.body);
+      const { email, password, confirmPassword } = req.body;
+
+      if (password !== confirmPassword) {
+        throw { name: "Passwords do not match" };
+      }
+
+      const newUser = await User.create({ email, password });
+
+      res.status(201).json({
+        statusCode: 201,
+        data: {
+          id: newUser.id,
+          email: newUser.email,
+        },
+      });
     } catch (error) {
       next(error);
     }
--- server/routers/api/auth/index.js
+++ server/routers/api/auth/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const AuthController = require("../../../controllers/auth");
 
-router.post("/", AuthController.register);
-router.post("/", AuthController.login);
+router.post("/register", AuthController.register);
+router.post("/login", AuthController.login);
 
 module.exports = router;
